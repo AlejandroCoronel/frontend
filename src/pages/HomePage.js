2b6dbfdd6406fcc8e31d5a6744e6c383b281a234
Alejandro Coronel <alejandrosabalero@gmail.com>
2022-03-16 18:17:05 -0300

    realizado hasta el HomePage pag 10 del material

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,7 +2,24 @@
 
  const HomePage = (props) => {
      return (
-         <div>HomePage</div>
+         <main className='holder'>
+            <div className='homeimg'>
+                <img src='images/home/img01.jpg' alt='avion'/>
+            </div>
+            <div className='columnas'>
+                <div className='bienvenidos left'>
+                    <h2>Biemvenidos</h2>
+                    <p>lksdf lsdfk lñaskd fl sdfl sd lsdfk jsldfkjasfñ asl  aslf lasdkfj asldkf</p>
+                </div>
+                <div className='testimonios right'>
+                    <h2>Testimonio</h2>
+                    <div className='testimonio'>
+                        <span className='cita'>Simplemente excelente</span>
+                        <span className='autor'>Juan Perez</span>
+                    </div>
+                </div>
+            </div>
+         </main>
      );
  }
 
